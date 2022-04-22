de1b70f73dc3df3fee7c53004e60ab8fa5060da3
Sergii Vdovareyze <servdo@gmail.com>
2022-04-22 08:17:00 +0300

    icons, links, design

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 (()=>{
     const CARD_NUMBER = '1234 5678 3456 7895';
+    let resetId
+
+    const copyNumber = (block) => {
+        clearTimeout(resetId);
+        block.classList.add('copied')
+        navigator.clipboard.writeText(CARD_NUMBER.replaceAll(/\s/g, ""))
+        resetId = setTimeout(() => {
+            resetCopy(block)
+        }, 10000)
+    }
+
+    const resetCopy = (block) => {
+        block.classList.remove('copied')
+    }
+
     const initCopy = () => {
         const block = document.getElementById('card-number-block')
         block.onclick = () => {
-            console.log('clicked')
-            navigator.clipboard.writeText(CARD_NUMBER.replaceAll(/\s/g, ""))
-            block.classList.add('copied')
+            copyNumber(block)
         }
-
     }
 
     initCopy()
